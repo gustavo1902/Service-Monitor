3d3314f60c51a1451444cc79718495e5191ed9ec
gustavo1902 <gustavomorais1009@gmail.com>
2025-06-09 22:11:40 -0300

    feat: monitor target

--- service-monitor-api/src/main/java/com/projeto/service_monitor/model/MonitorTarget.java
+++ service-monitor-api/src/main/java/com/projeto/service_monitor/model/MonitorTarget.java
@@ -1,5 +1,32 @@
 package com.projeto.service_monitor.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "monitor_targets")
+@Data // Lombok
 public class MonitorTarget {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; 
+
+    @Column(nullable = false, unique = true)
+    private String url; 
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.UNKNOWN;
+
+    private LocalDateTime lastCheck;
+
+    private String lastErrorMessage;
+
+    public enum Status {
+        UP, DOWN, UNKNOWN
+    }
 }
