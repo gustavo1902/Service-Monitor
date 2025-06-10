b33bf78a946277b76018e93549cca8d74d7f41aa
gustavo1902 <gustavomorais1009@gmail.com>
2025-06-09 22:14:35 -0300

    feat: habilitar agendamentos

--- service-monitor-api/src/main/java/com/projeto/service_monitor/ServiceMonitorApplication.java
+++ service-monitor-api/src/main/java/com/projeto/service_monitor/ServiceMonitorApplication.java
@@ -2,12 +2,13 @@ package com.projeto.service_monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; 
 
 @SpringBootApplication
+@EnableScheduling 
 public class ServiceMonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceMonitorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMonitorApplication.class, args);
+    }
 }
