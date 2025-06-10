ae1d5bb6d02b2a45c324498cb37debd84f1db052
gustavo1902 <gustavomorais1009@gmail.com>
2025-06-09 22:12:45 -0300

    feat: interface

--- service-monitor-api/src/main/java/com/projeto/service_monitor/repository/MonitorTargetRepository.java
+++ service-monitor-api/src/main/java/com/projeto/service_monitor/repository/MonitorTargetRepository.java
@@ -1,5 +1,9 @@
 package com.projeto.service_monitor.repository;
 
-public class MonitorTargetRepository {
-    
+import com.projeto.service_monitor.model.MonitorTarget;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MonitorTargetRepository extends JpaRepository<MonitorTarget, Long> {
 }
